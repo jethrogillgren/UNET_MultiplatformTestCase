53a45d45e31dc3b5e578c161b15c1b82e93839f2
jethro <jethrogillgren@gmail.com>
2018-03-07 11:39:15 +0000

    update

---
+++ Assets/TestGameObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TestGameObject : NetworkBehaviour {
+
+	// Use this for initialization
+	public override void OnStartServer () {
+        base.OnStartServer();
+        InvokeRepeating("Call", 2.0f, 2.0f);
+	}
+	
+	// Update is called once per frame
+	void Call() {
+        RpcDoStuff();
+	}
+
+    [ClientRpc]
+    public void RpcDoStuff()
+    {
+        Debug.Log("Stuff");
+#if UNITY_ANDROID
+			AndroidHelper.ShowAndroidToastMessage ( "Doing STUFF" );
+#endif
+    }
+}
